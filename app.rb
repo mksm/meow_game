161799329d4627b6670b4c339fe4b1235e3d4ea1
Diogo K <diogo.kamioka@gmail.com>
2015-11-10 18:45:26 -0200

    Fixing memcache dalli accessor

--- app.rb
+++ app.rb
@@ -7,23 +7,23 @@ set :cache, Dalli::Client.new
 INTERVAL=ENV["INTERVAL"]
 
 def too_soon?
-  interval = Time.now - cache.get("last_meow")
+  interval = Time.now - settings.cache.get("last_meow")
   return interval <= INTERVAL
 end
 
 def update_points(user_name)
-  actual = cache.get(user_name) || 0
-  cache.set(user_name, actual+1)
+  actual = settings.cache.get(user_name) || 0
+  settings.cache.set(user_name, actual+1)
   return actual+1
 end
 
 post '/meow' do
   content_type :json
   if too_soon? 
-    cache.set("last_meow", Time.now)
+    settings.cache.set("last_meow", Time.now)
     return { text: "Hey, wait! It's still too soon to meow. Now the timer has been reset! Wait more #{INTERVAL} seconds before meow." }.to_json
   else 
-    cache.set("last_meow", Time.now)
+    settings.cache.set("last_meow", Time.now)
     current_winner = params["user_name"]
     current_points = update_points(current_winner)
     {text: "Congratz, #{current_winner}! You are the current meower. You have #{current_points}"}.to_json
