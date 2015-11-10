e6842e5ab626984c5211037344ca613183054fbd
Diogo K <diogo.kamioka@gmail.com>
2015-11-10 19:02:12 -0200

    fix

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'json'
 require 'dalli'
-cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+
+@cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
                     {:username => ENV["MEMCACHIER_USERNAME"],
                      :password => ENV["MEMCACHIER_PASSWORD"],
                      :failover => true,
@@ -13,23 +14,23 @@ cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
 INTERVAL=ENV["INTERVAL"]
 
 def too_soon?
-  interval = Time.now - cache.get("last_meow")
+  interval = Time.now - @cache.get("last_meow")
   return interval <= INTERVAL
 end
 
 def update_points(user_name)
-  actual = cache.get(user_name) || 0
-  cache.set(user_name, actual+1)
+  actual = @cache.get(user_name) || 0
+  @cache.set(user_name, actual+1)
   return actual+1
 end
 
 post '/meow' do
   content_type :json
   if too_soon? 
-    cache.set("last_meow", Time.now)
+    @cache.set("last_meow", Time.now)
     return { text: "Hey, wait! It's still too soon to meow. Now the timer has been reset! Wait more #{INTERVAL} seconds before meow." }.to_json
   else 
-    cache.set("last_meow", Time.now)
+    @cache.set("last_meow", Time.now)
     current_winner = params["user_name"]
     current_points = update_points(current_winner)
     {text: "Congratz, #{current_winner}! You are the current meower. You have #{current_points}"}.to_json
