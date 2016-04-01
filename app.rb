365ba59b57e89817d55aa785ddbb425c68c5d480
mksm <mksm@iddqd.su>
2016-03-31 22:52:04 -0300

    Added meow_command "ranking"; points are integers

--- app.rb
+++ app.rb
@@ -7,14 +7,8 @@ set :cache, Dalli::Client.new(ENV["MEMCACHIER_SERVERS"], {
   password: ENV["MEMCACHIER_PASSWORD"]
 })
 
-# User list hash
-# {
-#   username_1: {points: float, last_meow: Time},
-#   username_2: {points: float, last_meow: Time}
-# }
-
 # define with: heroku config:set INTERVAL=<seconds>
-INTERVAL = ENV.fetch('INTERVAL', 6).to_i
+INTERVAL = ENV.fetch('INTERVAL', 600).to_i
 
 def last_meow
   settings.cache.get('last_meow') || Time.now
@@ -25,18 +19,16 @@ def too_soon?
 end
 
 def update_points(user_name, points)
-  if user_list = settings.cache.get('user_list') # would be false only the first time, when there is no memcached object
-    if user_list[user_name]
-      user_list[user_name][:points] += 1 
-    else
-      user_list[user_name] = { points: points, last_meow: Time.now }    
-    end
-  else
-    user_list = {}
-    user_list[user_name] = { points: points, last_meow: Time.now }    
-  end
-  settings.cache.set('user_list', user_list)
-  user_list[user_name][:points]
+  actual = settings.cache.get(user_name) || 0
+  settings.cache.set(user_name, actual + points)
+  actual + points
+end
+
+def mark_user_as_known(user_name)
+  known_users = settings.cache.get('known_users') || []
+  known_users << user_name
+  known_users = known_users.uniq
+  settings.cache.set('known_users', known_users)
 end
 
 post '/meow' do
@@ -46,9 +38,12 @@ post '/meow' do
     settings.cache.set('last_meow', Time.now)
     { text: "Hey, wait! It's still too soon to meow. Now the timer has been reset! Wait more #{INTERVAL} seconds before meow." }.to_json
   else
-    elapsed_time = Time.now - last_meow
+    elapsed_time = (Time.now - last_meow).to_i
     current_winner = params['user_name']
     current_points = update_points(current_winner, elapsed_time)
+
+    # add user to known_users array
+    mark_user_as_known(current_winner)
 
     settings.cache.set("last_meow", Time.now)
     { text: "Congratz, #{current_winner}! You are the current meower. You have #{current_points} points"}.to_json
@@ -56,31 +51,37 @@ post '/meow' do
 end
 
 post '/meow_command' do
-  content_type :json  
-  if params["text"] == 'ranking' 
-    user_list = settings.cache.get('user_list')
-    ranking_message = {
-      response_type: "in_channel", 
-      text: "Ranking atualizado dos meow'ers:",
-      attachments: [
+  content_type :json
+
+  if params['text'] == 'ranking'
+    msg = {
+      response_type: 'in_channel',
+      text:          'Ranking atualizado dos meow\'ers:',
+      attachments:   [
         {
           fields: [
             {
-              title: "Nick",
+              title: 'Nick',
               short: true
-              },
-              {
-                title: "Pontos",
-                short: true
-              }            
-            ]
-          }
-        ]
-      }
-    user_list.sort_by {|k,v| v[:points]}.each do |x|
-      ranking_message[:attachments][0][:fields] << { value: "#{x[0]}", short: true }
-      ranking_message[:attachments][0][:fields] << { value: "#{x[1][:points]}", short: true }      
-     end
-    return ranking_message.to_json
+            },
+            {
+              title: 'Pontos',
+              short: true
+            }
+          ]
+        }
+      ]
+    }
+
+    known_users = settings.cache.get('known_users') || []
+    scores = known_users.map { |user| [user, settings.cache.get(user)] }
+    scores = scores.sort_by(&:last).reverse
+
+    scores.each do |user, points|
+      msg[:attachments][0][:fields] << { value: user,   short: true }
+      msg[:attachments][0][:fields] << { value: points, short: true }
+    end
+
+    msg.to_json
   end
 end
